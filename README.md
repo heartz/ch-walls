f7c75a28b80052cc02876509641185520f0bbcc6
Ravi <upman16@gmail.com>
2014-10-04 19:27:30 +0530

    Initial commit + Wallpapers from directories works

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ ch-walls
 ========
 
 A simple shell script to switch between wallpapers from a specified directory on Gnome
+
+Edit the configuration section in wall.sh to set the directory where the pictures for the wallpaper are present and also the interval between switches.
+[Ensure line number 3 in the script is left blank]
+
+Run the script with
+$sh wall.sh
+
+To stop the switching, run
+$sh wall.sh stop
+
+
---
+++ wall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Script to randomly set Background from files in a directory
+CRON_SET=1
+#-----------------------------------------------------------------------
+#CONFIGURATION
+#Path to directory containing images
+DIR="/home/$USER"
+#Interval to change wallpaper in minutes
+INTERVAL=1
+#-----------------------------------------------------------------------
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ "$1" = "stop" ]; then
+	LINE=$(crontab -l | grep -n $0 | cut -f1 -d: > /dev/null)
+	crontab -l | sed `echo $LINE`d | crontab -
+	sed -i '3s/.*//' $SCRIPT_DIR/$0
+	exit
+fi
+#Cron runs with limited environment variables apparently adn DBUS_SESSION_BUS_ADDRESS is needed for gsettings
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
+PIC=$(ls $DIR/* | egrep -i '*.(png|jpg|jpeg)' | shuf -n1)
+# Command to set Background Image
+gsettings set org.gnome.desktop.background draw-background false && gsettings set org.gnome.desktop.background picture-uri file:///$PIC && gsettings set org.gnome.desktop.background draw-background true
+
+#Setting up a cron job
+if [ -z $CRON_SET ]; then
+	crontab -l | (cat; echo "*/$INTERVAL * * * * sh $SCRIPT_DIR/$0") | crontab -
+	sed -i '3s/.*/CRON_SET=1/' $SCRIPT_DIR/$0
+fi
+
